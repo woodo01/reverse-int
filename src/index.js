166415d0d3d6b4caf5cef46e8fe6d339f995a932
ivan.sigaev <ivan.sigaev@check24.de>
2024-08-20 13:37:21 +0200

    feat: add the second version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-    return parseInt(n.toString().split('').reverse().join(''));
+module.exports = function reverse (number) {
+    const reversedString = Math.abs(number).toString().split('').reverse().join('');
+    return parseInt(reversedString, 10);
 }
